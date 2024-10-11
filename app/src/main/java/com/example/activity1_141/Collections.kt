0e5de3a9267f1e7715ad54d334ea63b406c282dc
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-10-11 21:08:48 +0700

    List Mutable

--- app/src/main/java/com/example/activity1_141/Collections.kt
+++ app/src/main/java/com/example/activity1_141/Collections.kt
@@ -3,5 +3,11 @@ package com.example.activity1_141
 fun ContohList() {
     println("=== List ===")
     //List Read - Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    //List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
 
 }
