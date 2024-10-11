93968867d80142d886c1172bb98e579bbcc23314
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-10-11 21:14:25 +0700

    Menambahkan data ke dalam List Mutable

--- app/src/main/java/com/example/activity1_141/Collections.kt
+++ app/src/main/java/com/example/activity1_141/Collections.kt
@@ -10,4 +10,8 @@ fun ContohList() {
     val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
     println(shape)
 
+    //Menambahkan data ke dalam List Mutable
+    shape.add("Circle")
+    println(shape)
+
 }
