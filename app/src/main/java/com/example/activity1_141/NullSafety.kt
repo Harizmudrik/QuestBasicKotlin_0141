205fbe948bbc8da503380352bbf4270b751464ce
Harizmudrik <muhammadharizmudrik@gmail.com>
2024-10-11 23:22:00 +0700

    notNull tidak menerima nilai dari null

--- app/src/main/java/com/example/activity1_141/NullSafety.kt
+++ app/src/main/java/com/example/activity1_141/NullSafety.kt
@@ -24,5 +24,10 @@ fun nullSafety() {
     println(neverNull.length) //18
     println(nullable?.length) //null
 
-
+    //notNull doesn't accept null values
+    fun strLength(notNull: String): Int {
+        return notNull.length
+    }
+     println(strLength(neverNull)) //18
 }
+
